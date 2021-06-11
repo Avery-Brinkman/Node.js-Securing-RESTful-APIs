8f1bf6bfbe95c38f606876d228993bc7a85359da
Avery Brinkman <brinkmae@mail.uc.edu>
2021-06-11 18:16:35 -0400

    JWT setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import jwt from 'jsonwebtoken';
 // import bodyParser from 'body-parser';
 import routes from './src/routes/crmRoutes';
 
@@ -19,6 +20,28 @@ mongoose.connect(
 // bodyparser setup
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// jwt setup
+app.use((req, res, next) => {
+    if (
+        req.headers &&
+        req.headers.authorization &&
+        req.headers.authorization.split(' ')[0] === 'JWT'
+    ) {
+        jwt.verify(
+            req.headers.authorization.split(' ')[1],
+            'RESTFULAPIs',
+            (err, decode) => {
+                if (err) req.user = undefined;
+                req.user = decode;
+                next();
+            }
+        );
+    } else {
+        req.user = undefined;
+        next();
+    }
+});
 
 routes(app);
 
