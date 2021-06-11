6ae67f8f749fe7719477bf78b26025805ebe4b77
Avery Brinkman <brinkmae@mail.uc.edu>
2021-06-11 17:52:28 -0400

    Login handler

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -28,3 +28,38 @@ export const register = (req, res) => {
         }
     });
 };
+
+export const login = (req, res) => {
+    User.findOne(
+        {
+            email: req.body.email,
+        },
+        (err, user) => {
+            if (err) throw err;
+            if (!user) {
+                return res.status(401).json({
+                    message: 'Authentication failed. No user found.',
+                });
+            } else if (user) {
+                if (
+                    !user.comparePassword(req.body.password, user.hashPassword)
+                ) {
+                    return res.status(401).json({
+                        message: 'Authentication failed. Wrong password.',
+                    });
+                } else {
+                    return res.json({
+                        token: jwt.sign(
+                            {
+                                email: user.email,
+                                username: user.username,
+                                _id: user.id,
+                            },
+                            'RESTFULAPIs'
+                        ),
+                    });
+                }
+            }
+        }
+    );
+};
